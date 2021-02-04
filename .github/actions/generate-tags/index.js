77481440daaf09e09881188cac76afebf1c38d1c
Matthew Weier O'Phinney <matthew@weierophinney.net>
2021-02-04 14:58:06 -0600

    qa: no prefixes on generated tags
    - Removes the "v" prefix on tag names
    - Force create and push generated tag names (to allow overwriting)
    
    Signed-off-by: Matthew Weier O'Phinney <matthew@weierophinney.net>

--- .github/actions/generate-tags/index.js
+++ .github/actions/generate-tags/index.js
@@ -8,8 +8,8 @@ let major = semver.major(tag);
 let minor = major + '.' + semver.minor(tag);
 
 let tags = [
-    'v' + major,
-    'v' + minor,
+    major,
+    minor,
 ];
 
 core.info(`Tags: ${JSON.stringify(tags)}`);
