d5df1f8c59c2c0701059f51fdd40805c98910e8b
Maximilian Bösing <2189546+boesing@users.noreply.github.com>
2022-06-28 16:17:30 +0200

    qa: remove unnecessary annotations
    Signed-off-by: Maximilian Bösing <2189546+boesing@users.noreply.github.com>

--- src/config/composer.ts
+++ src/config/composer.ts
@@ -2,19 +2,8 @@ export interface ComposerRequirements {
     [key: string]: string
 }
 
-/**
- * @title Package
- */
 export interface ComposerJson {
-    /**
-     * @description This is a hash of package name (keys) and version constraints (values) that are required to run this package.
-     */
     require?: ComposerRequirements
-
-    /**
-     * @description This is a hash of package name (keys) and version constraints (values) that this package requires for developing it (testing tools and such).
-     */
     'require-dev'?: ComposerRequirements
-
     [key: string]: unknown
 }
