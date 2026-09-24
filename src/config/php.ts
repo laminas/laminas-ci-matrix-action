0fafee56c974fb775389c4693761bd73df583825
George Steel <george@net-glue.co.uk>
2026-09-24 10:43:16 +0100

    Add support for PHP 8.6
    Signed-off-by: George Steel <george@net-glue.co.uk>

--- src/config/php.ts
+++ src/config/php.ts
@@ -10,6 +10,7 @@ export const PHP_82 = '8.2';
 export const PHP_83 = '8.3';
 export const PHP_84 = '8.4';
 export const PHP_85 = '8.5';
+export const PHP_86 = '8.6';
 
 export const CURRENT_STABLE = PHP_81;
 export const CONTAINER_DEFAULT_PHP_VERSION = '@default';
@@ -31,6 +32,7 @@ export const INSTALLABLE_VERSIONS = [
     PHP_83,
     PHP_84,
     PHP_85,
+    PHP_86,
     CONTAINER_DEFAULT_PHP_VERSION,
 ] as const;
 
